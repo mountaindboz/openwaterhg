55bafca30f0b61e650f890fed7c1f0486b45ad9b
Dave Bosworth <David.Bosworth@water.ca.gov>
2021-02-21 09:45:55 -0800

    Updating package documentation

--- R/openwaterhg-package.R
+++ R/openwaterhg-package.R
@@ -1,14 +1,8 @@
-#' Analysis Tools and Data For DWR's Open Water Mercury Studies
-#'
-#' A collection of commonly-used functions and datasets that were used for data analysis
-#'     and visualizations of DWR's Open Water Mercury Studies. These studies include the
-#'     Yolo Bypass Mass Balance Study and three Vegetation Senescence studies.
-#'
 #' @keywords internal
-#' @docType package
-#' @name openwaterhg
 "_PACKAGE"
 
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
 ## usethis namespace: start
 ## usethis namespace: end
 NULL
